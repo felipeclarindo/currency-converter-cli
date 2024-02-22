0e7e3b46758cad66a5ad074b82005b0be183e599
felipeclarindo <fglpc2@gmail.com>
2024-02-22 11:30:17 -0300

    Initial Commit

---
+++ src/__init__.py
@@ -0,0 +1,29 @@
+"""
+Módulo src
+
+Este módulo fornece funcionalidades para o Conversor de Moedas.
+
+Funcionalidades disponíveis:
+- Função de limpeza de tela cross-platform.
+- Classe Main para execução do Conversor de Moedas.
+
+Para utilizar este pacote, importe as funcionalidades desejadas da seguinte maneira:
+
+from src import clear, Main
+
+Exemplo de uso:
+
+# Limpa a tela do console
+clear(True)
+
+# Instância da classe Main
+conversor = Main()
+
+# Executa o Conversor de Moedas
+conversor.conversor_de_moedas()
+"""
+
+__version__ = '1.0.0'
+
+
+
---
+++ src/conversor/__init__.py
@@ -0,0 +1,22 @@
+"""
+Pacote Conversor
+
+Este pacote fornece funcionalidades para converter valores entre diferentes moedas.
+
+Funcionalidades disponíveis:
+- Classe Conversor para realizar conversões entre moedas.
+
+Para utilizar este pacote, importe as funcionalidades desejadas da seguinte maneira:
+
+from conversor import Conversor
+
+Exemplo de uso:
+
+# Cria uma instância do Conversor
+conversor = Conversor(moeda1=1, moeda2=2, valor=100)
+
+# Realiza uma conversão de USD para EUR
+conversor.conversao("USD", "EUR")
+"""
+
+__version__ = '1.0.0'
