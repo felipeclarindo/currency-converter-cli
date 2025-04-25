felipeclarindo/currency-converter

feat(application): Improving the system, fixing bugs and implementing POO fixes #2 (#3)

main.py
@@ -1,0 +1,4 @@
+from app.app import App
+
+app = App()
+app.run()

felipeclarindo/currency-converter-cli

main.py
@@ -1,0 +1,4 @@
+from src.app import App
+
+app = App()
+app.run()